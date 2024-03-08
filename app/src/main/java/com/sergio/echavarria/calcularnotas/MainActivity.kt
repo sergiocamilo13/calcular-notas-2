22f7c9d169070834241280ba4b86a8bec50b84df
sergiocamilo13 <sergiocamilo13102022@gmail.com>
2024-03-08 08:41:34 -0500

    sergio

--- app/src/main/java/com/sergio/echavarria/calcularnotas/MainActivity.kt
+++ app/src/main/java/com/sergio/echavarria/calcularnotas/MainActivity.kt
@@ -2,9 +2,11 @@ package com.sergio.echavarria.calcularnotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var siguienteNota: Button
     private lateinit var Finalizar: Button
     private lateinit var progreso: ProgressBar
+    private lateinit var vistaPromedioFinal : TextView
+    private lateinit var vistaNotaFinal : TextView
+    private lateinit var Reiniciar : Button
+
 
     var porcentajeAcumulado  = 0
 
@@ -32,8 +38,46 @@ class MainActivity : AppCompatActivity() {
         siguienteNota = findViewById(R.id.siguiente_nota)
         Finalizar = findViewById(R.id.Finalizar)
         progreso = findViewById(R.id.progressBar)
+        vistaPromedioFinal = findViewById(R.id.vistaPromedioFinal)
+        vistaNotaFinal = findViewById(R.id.vistaNotaFinal)
+        Reiniciar = findViewById(R.id.Reiniciar)
+
+        Reiniciar.setOnClickListener {
+
+            vistaNotaFinal.visibility = View.GONE
+
+            vistaPromedioFinal.visibility = View.GONE
+
+            Finalizar.visibility = View.GONE
+
+            Reiniciar.visibility = View.GONE
+
+            nombreIngresado.text.clear()
+            notasIngresadas.text.clear()
+            porcentajesIngresados.text.clear()
+            progreso.progress = 0
+            nombreIngresado.isEnabled = true
+            vistaPromedioFinal.text = ""
+            vistaNotaFinal.text = ""
+            porcentajeAcumulado = 0
+        }
+
+        Finalizar.setOnClickListener {
+
+            Reiniciar.visibility = View.VISIBLE
+
+            vistaNotaFinal.visibility = View.VISIBLE
+
+            vistaPromedioFinal.visibility = View.VISIBLE
+
+            vistaNotaFinal.text = "nota final : " + porcentajeAcumulado
+
+            vistaPromedioFinal.text = "promedio : " + calcularpromedio()
+        }
+
 
         siguienteNota.setOnClickListener {
+
             val nombre = nombreIngresado.text.toString()
             val nota = notasIngresadas.text.toString()
             val porcentaje = porcentajesIngresados.text.toString()
@@ -87,5 +131,29 @@ class MainActivity : AppCompatActivity() {
     }
     fun validarPorcentaje(porcentaje : Int ) : Boolean{
         return porcentajeAcumulado + porcentaje <=100
+    }
+    fun calcularpromedio(): Double{
+
+        var p = 0.0
+
+        for (n in listaDeNotas){
+
+            p += n
+
+        }
+        return Math.round((p/ listaDeNotas.size) * 1000.0) / 1000.0
+    }
+    fun notafinal(): Double {
+
+        var notaFinal : Double = 0.0
+
+        var contador = 0
+
+        for (n in listaDeNotas){
+
+            notaFinal += (n * listaDePorcentaje[contador]) / 100
+            contador++
+        }
+        return Math.round(notaFinal * 1000.0) / 1000.0
     }
 }
